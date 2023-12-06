b1847cc3440bc1de7b729dabe58c92fbc76cf028
Fruitful Ejiro <frutfl@outlook.com>
2023-12-06 15:12:56 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+/** @format */
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const PORT = 8080;
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT);
+console.log(`Server is listening of port ${PORT}`);
